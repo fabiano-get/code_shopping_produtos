e3826d5eabff444a294c851c548150a74e4f38bb
fabiano-get <fabiano@getsystem.com.br>
2018-11-22 11:49:35 -0200

    autenticação jwt

--- routes/api.php
+++ routes/api.php
@@ -19,20 +19,28 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 
 
 Route::group(['namespace' => 'Api', 'as' => 'api.'], function () {
-    Route::patch('products/{product}/restore', 'ProductController@restore');
-    Route::resource('products', 'ProductController', ['except' => ['create', 'edit']]);
-    Route::resource('categories','CategoryController', ['except' => ['create', 'edit']]);
-    //recurso filho
+    Route::name('login')->post('login', 'AuthController@login');
+    Route::name('refresh')->post('refresh','AuthController@refresh');
+
+    Route::group(['middleware' => ['auth:api','jwt.refresh']], function() {
+        Route::name('logout')->post('logout','AuthController@logout');
+        Route::name('me')->get('me', 'AuthController@me');
+        Route::patch('products/{product}/restore', 'ProductController@restore');
+        Route::resource('products', 'ProductController', ['except' => ['create', 'edit']]);
+        Route::resource('categories','CategoryController', ['except' => ['create', 'edit']]);
+        //recurso filho
 //    POST products/1/categories
 //    PUT products/1/categories  ADICIONAR E REMOVER -> [1,3] [10 CATEGORIAS]
 //    GET products/1/categories
 //    DELETE products/1/categories
 //    DELETE products/1/categories/10
-    Route::resource('products.categories','ProductCategoryController', ['only' => ['index','store','destroy']]);
-    Route::resource('products.photos','ProductPhotoController', ['except' => ['create', 'edit']]);
-    Route::resource('inputs','ProductInputController', ['only' => ['index','store','show']]);
-    Route::resource('outputs','ProductOutputController', ['only' => ['index','store','show']]);
-    Route::resource('users','UserController', ['except' => ['create', 'edit']]);
-    // GET products/{product}/categories
-    //
+        Route::resource('products.categories','ProductCategoryController', ['only' => ['index','store','destroy']]);
+        Route::resource('products.photos','ProductPhotoController', ['except' => ['create', 'edit']]);
+        Route::resource('inputs','ProductInputController', ['only' => ['index','store','show']]);
+        Route::resource('outputs','ProductOutputController', ['only' => ['index','store','show']]);
+        Route::resource('users','UserController', ['except' => ['create', 'edit']]);
+        // GET products/{product}/categories
+        //
+
+    });
 });
